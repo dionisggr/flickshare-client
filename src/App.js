1f46f6798d0dc5ebfdd6ad00eb87ab2539c1a28f
Dio Gonzalez <dionisggr@gmail.com>
2021-02-09 20:43:18 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <main className='App'>
+        {/* content goes here */}
+      </main>
+    );
+  };
+};
+
+export default App;

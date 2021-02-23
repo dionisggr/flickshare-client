369e770b9d4adc4bd6d5e33a264e68a9dfb4e99a
Dio Gonzalez <dionisggr@gmail.com>
2021-02-23 13:27:25 -0700

    config changes

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const config = {
-  API_URL: process.env.REACT_APP_API_URL || 'http://localhost:8000/api',
+  API_URL: process.env.REACT_APP_API_URL || 'https://cryptic-badlands-24275.herokuapp.com/api',
   API_KEY: process.env.REACT_APP_API_KEY || 'my-secret-admin',
   API_ADMIN_KEY: process.env.REACT_APP_API_ADMIN_KEY || 'my-secret-admin',
   JWT_SECRET: process.env.REACT_APP_JWT_SECRET || 'my-secret-jwt',

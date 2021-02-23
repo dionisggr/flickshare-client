c1e5f88d4111f93f200494ef5026b8694e73e854
Dio Gonzalez <dionisggr@gmail.com>
2021-02-23 12:48:34 -0700

    deployment changes

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ const config = {
   API_KEY: process.env.REACT_APP_API_KEY || 'my-secret-admin',
   API_ADMIN_KEY: process.env.REACT_APP_API_ADMIN_KEY || 'my-secret-admin',
   JWT_SECRET: process.env.REACT_APP_JWT_SECRET || 'my-secret-jwt',
-  TMDB_API_KEY: process.env.REACT_APP_TMDB_API_KEY || '',
+  TMDB_API_KEY: process.env.REACT_APP_TMDB_API_KEY || '59b42538d4bcf7fe1a42cf887d900824',
   TMDB_API_URL: process.env.REACT_APP_TMDB_API_URL || 'https://api.themoviedb.org/3'
 };
 

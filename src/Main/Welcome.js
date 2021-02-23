f8e01409139bc2ac1757d1e98f00d8a29e948d7d
Dio Gonzalez <dionisggr@gmail.com>
2021-02-23 12:13:07 -0700

    welcome instructions updated

--- src/Main/Welcome.js
+++ src/Main/Welcome.js
@@ -35,16 +35,15 @@ class Welcome extends React.Component {
         <label>Why struggle choosing a movie to watch?</label>
 
         <p>Good movie suggestions are hard to come by. Usually we rely on the Internet or our friends to decide what to watch next.</p>
-        <p>What if we could combine both?</p>
-        <p>What if we could create a list with our favorite movies and be suggested other movies based on your likes?</p>
-        <p>What if we could take this step further, and base suggestions on your friends lists as well?</p>
+        <p>What if we could just get some ideas from our own interests?</p>
+        <p>What if we could save these lists and edit them in the future for different results?</p>
 
         <label>It's simple:</label>
 
         <ol>
           <li>Create an account.</li>
-          <li>Create a list with your favorite movies.</li>
-          <li>Make new friends, watch other users' lists and send suggestions!</li>
+          <li>Create a list (or many!) with your favorite movies.</li>
+          <li>Hit the Get Suggestions! button</li>
           <li>Come back for more!</li>
         </ol>
 

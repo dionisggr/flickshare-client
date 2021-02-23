b67c0e10be98386cc8b670e497c6cd2a224f90b3
Dio Gonzalez <dionisggr@gmail.com>
2021-02-23 10:49:40 -0700

    tests passing again

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -4,9 +4,9 @@ function Footer() {
   return (
     <footer>
       <div className='icons'>
-        <a href="mailto:dionisggr@gmail.com" target="_blank" aria-label="email"><i class="fa fa-envelope" aria-hidden="true"></i></a>
-        <a href="https://github.com/dionisggr/flickshare-client/" target="_blank" aria-label="github"><i class="fa fa-github" aria-hidden="true"></i></a>
-        <a href="https://www.linkedin.com/in/dionis-gonzalez-ramirez/" target="_blank" aria-label="linked-in"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a>
+        <a href="mailto:dionisggr@gmail.com" target="_blank" aria-label="email"><i className="fa fa-envelope" aria-hidden="true"></i></a>
+        <a href="https://github.com/dionisggr/flickshare-client/" target="_blank" aria-label="github"><i className="fa fa-github" aria-hidden="true"></i></a>
+        <a href="https://www.linkedin.com/in/dionis-gonzalez-ramirez/" target="_blank" aria-label="linked-in"><i className="fa fa-linkedin-square" aria-hidden="true"></i></a>
       </div>
       
       <h2>&copy; Dionis González Ramírez</h2>

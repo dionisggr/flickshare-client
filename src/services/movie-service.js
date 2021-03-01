b146c4f2a36e791b019c2c09a3161470ed5bf346
Dio Gonzalez <dionisggr@gmail.com>
2021-02-28 19:34:45 -0700

    styling changes

--- src/services/movie-service.js
+++ src/services/movie-service.js
@@ -1,22 +1,25 @@
 const MovieService = {
   prepare: (values) => {
-    const movies = values.map(value => {
-      const {
-        id: tmdb_id,
-        overview: description,
-        vote_average: avg_vote,
-        popularity, release_date, poster_path,
-        title, original_title, vote_count
-      } = value;
-  
-      const movie = {
-        name: title || original_title,
-        poster: `https://image.tmdb.org/t/p/original/${poster_path}`,
-        description, release_date, popularity,
-        avg_vote, vote_count, tmdb_id, 
-      };  
-      return movie;
-    });
+    const movies = values
+      .filter(value => value.poster_path)
+      .map(value => {
+        const {
+          id: tmdb_id,
+          overview: description,
+          vote_average: avg_vote,
+          popularity, release_date, poster_path,
+          title, original_title, vote_count
+        } = value;
+    
+        const movie = {
+          name: title || original_title,
+          poster: `https://image.tmdb.org/t/p/original/${poster_path}`,
+          description, release_date, popularity,
+          avg_vote, vote_count, tmdb_id, 
+        };
+
+        return movie;
+      });
     
     return movies;
   }
